155d982cf27dce5d7913f1e2e7fa016d88dade43
samwhitford11 <samantha.fichte98@gmail.com>
2022-11-09 22:09:26 -0700

    if statement complete

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -6,20 +6,27 @@ const PORT = 3010;
 
 // Take one Down and Pass it Around
 
+// app.get('/:number_of_bottles', (request, response) =>{
+//     const numBottles = request.params.number_of_bottles
+//     const nextBottle = numBottles - 1
+    // console.log("99 bottles of beer on the wall")
+//     response.send(`<a href='/${nextBottle}'><button>take one down, pass it around</button></a> <br> <h1>${numBottles} number of bottle of beer on the wall</h1>`)
+// });
+
 app.get('/:number_of_bottles', (request, response) =>{
     const numBottles = request.params.number_of_bottles
     const nextBottle = numBottles - 1
-    // console.log("99 bottles of beer on the wall")
-    response.send(`<a href='/${nextBottle}'><button>take one down, pass it around</button></a> <h1>${numBottles} number of bottle of beer on the wall</h1> `)
-    // if(${numBottles} = 0){
-    //     response.send(`<a href='/${numBottles}'><button>Start Over</button></a>`)
-    // }
 
-    
+    if(request.params.number_of_bottles > 1) {
+        response.send(`<a href='/${nextBottle}'><button>Take one down, pass it around</button></a> <br> <h1>${numBottles} number of bottle of beer on the wall</h1>`);
 
-})
+    } else if (numBottles > 0) {
+        response.send(`<a href='/${nextBottle}'><button>Take one down and pass it around</button></a><h1>${numBottles} bottle beer on the wall.</h1></br>`); 
 
+    } else {
+        response.send(`<a href='/'><button>Start Over</button></a><h1>${numBottles} bottles of beer on the wall.</h1></br>`)
+    }
+});
 app.listen(PORT, () => {
     console.log(`Listing to port ${PORT}`)
-
 })
